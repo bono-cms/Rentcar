1a8624c15c76ca0c5d4934c96a4205994071d569
dave-yang <daworld.ny@gmail.com>
2019-04-09 23:10:08 +0500

    Initial files

---
+++ Module.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Rentcar;
+
+use Cms\AbstractCmsModule;
+
+final class Module extends AbstractCmsModule
+{
+    /**
+     * {@inhertiDoc}
+     */
+    public function getServiceProviders()
+    {
+        return array(
+            
+        );
+    }
+}
