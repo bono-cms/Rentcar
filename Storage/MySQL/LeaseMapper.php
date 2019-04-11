8972905b427eadbfc4f85bd0af32f08aea8c8f28
dave-yang <daworld.ny@gmail.com>
2019-04-11 20:19:41 +0500

    Lease mapper and its schema

---
+++ Storage/MySQL/LeaseMapper.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Rentcar\Storage\MySQL;
+
+use Cms\Storage\MySQL\AbstractMapper;
+use Rentcar\Storage\LeaseMapperInterface;
+
+final class LeaseMapper extends AbstractMapper implements LeaseMapperInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public static function getTableName()
+    {
+        return self::getWithPrefix('bono_module_rentcar_lease');
+    }
+}
