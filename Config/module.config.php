484ee917e1e22c230191bd6583fcddf502c0f716
dave-yang <daworld.ny@gmail.com>
2020-04-10 15:41:41 +0500

    Added booking menu items

--- Config/module.config.php
+++ Config/module.config.php
@@ -4,29 +4,41 @@
  * Module configuration container
  */
 
-return array(
+return [
     'caption'  => 'Cars',
     'description' => 'Cars module allows you to manage cars booking on your site',
     // Bookmarks of this module
-    'bookmarks' => array(
-        array(
+    'bookmarks' => [
+        [
             'name' => 'View all cars',
             'controller' => 'Rentcar:Admin:Car@indexAction',
             'icon' => 'fas fa-car'
-        )
-    ),
-    'menu' => array(
+        ]
+    ],
+
+    'menu' => [
         'name'  => 'Cars',
         'icon' => 'fas fa-car',
-        'items' => array(
-            array(
+        'items' => [
+            [
                 'route' => 'Rentcar:Admin:Car@indexAction',
                 'name' => 'View all cars'
-            ),
-            array(
+            ],
+
+            [
                 'route' => 'Rentcar:Admin:Lease@indexAction',
                 'name' => 'Car leasing'
-            )
-        )
-    )
-);
+            ],
+
+            [
+                'route' => 'Rentcar:Admin:Booking@indexAction',
+                'name' => 'Bookings'
+            ],
+
+            [
+                'route' => 'Rentcar:Admin:Booking@availabilityAction',
+                'name' => 'Availability graph'
+            ]
+        ]
+    ]
+];
