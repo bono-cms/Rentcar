9a265ca4411b338bb9695fc812c9623894adf120
dave-yang <daworld.ny@gmail.com>
2020-04-11 18:05:49 +0500

    Added getCheckout() and getCheckin()

--- Service/FinderEntity.php
+++ Service/FinderEntity.php
@@ -12,9 +12,51 @@
 namespace Rentcar\Service;
 
 use Krystal\Stdlib\VirtualEntity;
+use DateTime;
 
 final class FinderEntity extends VirtualEntity
 {
+    /**
+     * Normalizes date and time
+     * 
+     * @param string $date
+     * @param string $time
+     * @return string
+     */
+    private static function normalizeTime($date, $time)
+    {
+        $time = new DateTime($time);
+
+        $hour = $time->format('H');
+        $minute = $time->format('i');
+        $second = $time->format('s');
+
+        $date = new DateTime($date);
+        $date->setTime($hour, $minute, $second);
+
+        return $date->format('Y-m-d H:i:s');
+    }
+
+    /** 
+     * Returns checkout date and time
+     * 
+     * @return string
+     */
+    public function getCheckout()
+    {
+        return self::normalizeTime($this->getReturnDate(), $this->getReturnTime());
+    }
+
+    /** 
+     * Returns checkout date and time
+     * 
+     * @return string
+     */
+    public function getCheckin()
+    {
+        return self::normalizeTime($this->getPickupDate(), $this->getPickupTime());
+    }
+
     /**
      * Checks if entire entity is populated
      * 
